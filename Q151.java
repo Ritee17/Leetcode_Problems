07b2fc5f6d87c15ab76d5334edb8eb41e42c5819
Ritee <149651104+Ritee17@users.noreply.github.com>
2025-07-08 20:13:39 +0530

    Add files via upload

---
+++ Q151.java
@@ -0,0 +1,21 @@
+package DSA.Leetcode;
+import java.util.Scanner;
+
+public class Q151{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(s);
+        System.out.println(s.trim());
+    }
+    public static String Reverse_words(String s){
+        s = s.trim();
+        String[] arr = s.split("\s+");
+        String and = "";
+        for(int i=arr.length-1 ; i>=0 ; i--){
+            ans + = arr[i]+ " ";
+        }
+        ans.trim();
+        return ans;
+    }
+}
