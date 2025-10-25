c5521cb6dc61cea763956da9960d4aec17d176d7
Ritee/17 <riteeravi13334@gmail.com>
2025-10-25 19:58:14 +0530

    recursion questions add

---
+++ Questions/Recursion_Factorial.java
@@ -0,0 +1,17 @@
+package DSA.Leetcode_Problems.Questions;
+
+public class Recursion_Factorial {
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(fact(n));
+    }
+    public static int fact(int n){
+        //  base case
+        if(n==1 || n==0){ 
+            return 1;
+        }
+        // recursive case
+        int fn = fact(n-1); // smaller problem
+        return fn*n;
+    }
+}
